87b511e1836a1c74ba6a1a10248f5257df573d8d
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <PfizerJunkie@protonmail.com>
2024-04-28 14:09:51 +0200

    Update protocol.h
    Commented each and every struct and it's members.

--- debugger/include/protocol.h
+++ debugger/include/protocol.h
@@ -1,13 +1,13 @@
-// golden
-// 6/12/2018
-//
-
+#pragma once
 #ifndef _PROTOCOL_H
 #define _PROTOCOL_H
 
-#include <ps4.h>
-#include "errno.h"
-#include "kdbg.h"
+#include "../../ps4-payload-sdk/include/types.h"
+#include "../../ps4-payload-sdk/include/network.h"
+#include "./pkt_sizes.h"
+#include "./errno.h"
+#include "./kdbg.h"
+
 
 #define PACKET_VERSION          "1.2"
 #define PACKET_MAGIC            0xFFAABBCC
@@ -23,6 +23,7 @@
 #define CMD_PROC_ELF            0xBDAA0007
 #define CMD_PROC_PROTECT        0xBDAA0008
 #define CMD_PROC_SCAN           0xBDAA0009
+#define CMD_PROC_CONSOLE_BASED_SCAN 0x38192
 #define CMD_PROC_INFO           0xBDAA000A
 #define CMD_PROC_ALLOC          0xBDAA000B
 #define CMD_PROC_FREE           0xBDAA000C
@@ -69,266 +70,277 @@
 
 #define CMD_FATAL_STATUS(s) ((s >> 28) == 15)
 
-struct cmd_packet {
-    uint32_t magic;
-    uint32_t cmd;
-    uint32_t datalen;
-    // (field not actually part of packet, comes after)
-    void *data;
-} __attribute__((packed));
 #define CMD_PACKET_SIZE 12
 
-// proc
+
+// Packet structure for communication between debugger and target
+struct cmd_packet {
+    uint32_t magic;     // Magic number identifying the packet
+    uint32_t cmd;       // Command identifier
+    uint32_t datalen;   // Length of data payload
+    void *data;         // Pointer to the data payload (not part of the packet, added after)
+} __attribute__((packed));
+
+// Packet for reading data from a process
 struct cmd_proc_read_packet {
-    uint32_t pid;
-    uint64_t address;
-    uint32_t length;
+    uint32_t pid;       // Process ID
+    uint64_t address;   // Address to read from
+    uint32_t length;    // Length of data to read
 } __attribute__((packed));
-#define CMD_PROC_READ_PACKET_SIZE 16
 
+// Packet for writing data to a process
 struct cmd_proc_write_packet {
-    uint32_t pid;
-    uint64_t address;
-    uint32_t length;
+    uint32_t pid;       // Process ID
+    uint64_t address;   // Address to write to
+    uint32_t length;    // Length of data to write
 } __attribute__((packed));
-#define CMD_PROC_WRITE_PACKET_SIZE 16
 
+// Packet for retrieving memory maps of a process
 struct cmd_proc_maps_packet {
-    uint32_t pid;
+    uint32_t pid;       // Process ID
 } __attribute__((packed));
-#define CMD_PROC_MAPS_PACKET_SIZE 4
 
+// Packet for installing a (hook?) in a process
 struct cmd_proc_install_packet {
-    uint32_t pid;
+    uint32_t pid;       // Process ID
 } __attribute__((packed));
+
+// Response structure for usage of the install packet
 struct cmd_proc_install_response {
-    uint64_t rpcstub;
+    uint64_t rpcstub;   // RPC (Remote procedure call) stub
 } __attribute__((packed));
-#define CMD_PROC_INSTALL_PACKET_SIZE 4
-#define CMD_PROC_INSTALL_RESPONSE_SIZE 8
 
+// Packet for making a remote procedure call (RPC) to a process
 struct cmd_proc_call_packet {
-    uint32_t pid;
-    uint64_t rpcstub;
-    uint64_t rpc_rip;
-    uint64_t rpc_rdi;
-    uint64_t rpc_rsi;
-    uint64_t rpc_rdx;
-    uint64_t rpc_rcx;
-    uint64_t rpc_r8;
-    uint64_t rpc_r9;
+    uint32_t pid;       // Process ID
+    uint64_t rpcstub;   // RPC (Remote procedure call) stub
+    uint64_t rpc_rip;   // RPC (Remote procedure call) rip
+    uint64_t rpc_rdi;   // RPC (Remote procedure call) rdi
+    uint64_t rpc_rsi;   // RPC (Remote procedure call) rsi
+    uint64_t rpc_rdx;   // RPC (Remote procedure call) rdx
+    uint64_t rpc_rcx;   // RPC (Remote procedure call) rcx
+    uint64_t rpc_r8;    // RPC (Remote procedure call) r8
+    uint64_t rpc_r9;    // RPC (Remote procedure call) r9
 } __attribute__((packed));
+
+// Response structure for after having made an RPC
 struct cmd_proc_call_response {
-    uint32_t pid;
-    uint64_t rpc_rax;
+    uint32_t pid;       // Process ID
+    uint64_t rpc_rax;   // RPC (Remote procedure call) rax
 } __attribute__((packed));
-#define CMD_PROC_CALL_PACKET_SIZE 68
-#define CMD_PROC_CALL_RESPONSE_SIZE 12
 
+// Packet for loading an ELF file into a process
 struct cmd_proc_elf_packet {
-    uint32_t pid;
-    uint32_t length;
+    uint32_t pid;       // Process ID
+    uint32_t length;    // Length
 } __attribute__((packed));
-#define CMD_PROC_ELF_PACKET_SIZE 8
 
+
+// Packet for changing memory protection of a region in a process
 struct cmd_proc_protect_packet {
-    uint32_t pid;
-    uint64_t address;
-    uint32_t length;
-    uint32_t newprot;
+    uint32_t pid;       // Process ID
+    uint64_t address;   // Address
+    uint32_t length;    // Length
+    uint32_t newprot;   // New protection
 } __attribute__((packed));
-#define CMD_PROC_PROTECT_PACKET_SIZE 20
 
-typedef enum cmd_proc_scan_valuetype {
-   valTypeUInt8 = 0,
-   valTypeInt8,
-   valTypeUInt16,
-   valTypeInt16,
-   valTypeUInt32,
-   valTypeInt32,
-   valTypeUInt64,
-   valTypeInt64,
-   valTypeFloat,
-   valTypeDouble,
-   valTypeArrBytes,
-   valTypeString
-} __attribute__((__packed__)) cmd_proc_scan_valuetype;
+// Enumeration for value types used in process scanning
+ enum cmd_proc_scan_valuetype {
+    valTypeUInt8 = 0,
+    valTypeInt8,
+    valTypeUInt16,
+    valTypeInt16,
+    valTypeUInt32,
+    valTypeInt32,
+    valTypeUInt64,
+    valTypeInt64,
+    valTypeFloat,
+    valTypeDouble,
+    valTypeArrBytes,
+    valTypeString
+} __attribute__((__packed__));
 
-typedef enum cmd_proc_scan_comparetype {
-   cmpTypeExactValue = 0,
-   cmpTypeFuzzyValue,
-   cmpTypeBiggerThan,
-   cmpTypeSmallerThan,
-   cmpTypeValueBetween,
-   cmpTypeIncreasedValue,
-   cmpTypeIncreasedValueBy,
-   cmpTypeDecreasedValue,
-   cmpTypeDecreasedValueBy,
-   cmpTypeChangedValue,
-   cmpTypeUnchangedValue,
-   cmpTypeUnknownInitialValue
-} __attribute__((__packed__)) cmd_proc_scan_comparetype;
+// Enumeration for comparison types used in process scanning
+enum cmd_proc_scan_comparetype {
+    cmpTypeExactValue = 0,
+    cmpTypeFuzzyValue,
+    cmpTypeBiggerThan,
+    cmpTypeSmallerThan,
+    cmpTypeValueBetween,
+    cmpTypeIncreasedValue,
+    cmpTypeIncreasedValueBy,
+    cmpTypeDecreasedValue,
+    cmpTypeDecreasedValueBy,
+    cmpTypeChangedValue,
+    cmpTypeUnchangedValue,
+    cmpTypeUnknownInitialValue
+} __attribute__((__packed__));
 
+// Structure for packet used in process scanning
 struct cmd_proc_scan_packet {
-   uint32_t pid;
-   uint8_t valueType;
-   uint8_t compareType;
-   uint32_t lenData;
+    uint32_t pid;           // Process ID
+    uint8_t valueType;      // Value type
+    uint8_t compareType;    // Comparison type
+    uint32_t lenData;       // Length of data
 } __attribute__((packed));
-#define CMD_PROC_SCAN_PACKET_SIZE 10
 
+// Structure for requesting information about a process
 struct cmd_proc_info_packet {
-    uint32_t pid;
+    uint32_t pid;   // Process ID
 } __attribute__((packed));
+
+// Response structure for process information request
 struct cmd_proc_info_response {
-    uint32_t pid;
-    char name[40];
-    char path[64];
-    char titleid[16];
-    char contentid[64];
+    uint32_t pid;           // Process ID
+    char name[40];          // Process name
+    char path[64];          // Process path
+    char titleid[16];       // Process title ID
+    char contentid[64];     // Process content ID
 } __attribute__((packed));
-#define CMD_PROC_INFO_PACKET_SIZE 4
-#define CMD_PROC_INFO_RESPONSE_SIZE 188
 
+// Structure for requesting memory allocation in a process
 struct cmd_proc_alloc_packet {
-    uint32_t pid;
-    uint32_t length;
+    uint32_t pid;       // Process ID
+    uint32_t length;    // Length
 } __attribute__((packed));
+
+// Response structure for memory allocation request
 struct cmd_proc_alloc_response {
-    uint64_t address;
+    uint64_t address;   // Address
 } __attribute__((packed));
-#define CMD_PROC_ALLOC_PACKET_SIZE 8
-#define CMD_PROC_ALLOC_RESPONSE_SIZE 8
 
+
+// Structure for requesting memory deallocation in a process
 struct cmd_proc_free_packet {
-    uint32_t pid;
-    uint64_t address;
-    uint32_t length;
+    uint32_t pid;       // Process ID
+    uint64_t address;   // Address
+    uint32_t length;    // Length
 } __attribute__((packed));
-#define CMD_PROC_FREE_PACKET_SIZE 16
 
-// debug
+
+// Structure for attaching debugger to a process
 struct cmd_debug_attach_packet {
-    uint32_t pid;
+    uint32_t pid;   // Process ID
 } __attribute__((packed));
-#define CMD_DEBUG_ATTACH_PACKET_SIZE 4
 
+
+// Structure for setting breakpoints
 struct cmd_debug_breakpt_packet {
-    uint32_t index;
-    uint32_t enabled;
-    uint64_t address;
+    uint32_t index;     // Index
+    uint32_t enabled;   // Enabled
+    uint64_t address;   // Address
 } __attribute__((packed));
-#define CMD_DEBUG_BREAKPT_PACKET_SIZE 16
 
+
+// Structure for setting watchpoints
 struct cmd_debug_watchpt_packet {
-    uint32_t index;
-    uint32_t enabled;
-    uint32_t length;
-    uint32_t breaktype;
-    uint64_t address;
+    uint32_t index;     // Index
+    uint32_t enabled;   // Enabled
+    uint32_t length;    // Length
+    uint32_t breaktype; // Break type
+    uint64_t address;   // Address
 } __attribute__((packed));
-#define CMD_DEBUG_WATCHPT_PACKET_SIZE 24
 
+// Structure for stopping a thread in debugging mode
 struct cmd_debug_stopthr_packet {
-    uint32_t lwpid;
+    uint32_t lwpid;     // LWP ID
 } __attribute__((packed));
-#define CMD_DEBUG_STOPTHR_PACKET_SIZE 4
 
+
+// Structure for resuming a thread in debugging mode
 struct cmd_debug_resumethr_packet {
-    uint32_t lwpid;
+    uint32_t lwpid;     // LWP ID
 } __attribute__((packed));
-#define CMD_DEBUG_RESUMETHR_PACKET_SIZE 4
 
+// Structure for requesting register values of a thread in debugging mode
 struct cmd_debug_getregs_packet {
-    uint32_t lwpid;
+    uint32_t lwpid;     // LWP ID
 } __attribute__((packed));
-#define CMD_DEBUG_GETREGS_PACKET_SIZE 4
 
+// Structure for setting register values of a thread in debugging mode
 struct cmd_debug_setregs_packet {
-    uint32_t lwpid;
-    uint32_t length;
+    uint32_t lwpid;     // LWP ID
+    uint32_t length;    // Length
 } __attribute__((packed));
-#define CMD_DEBUG_SETREGS_PACKET_SIZE 8
 
+// Structure for stopping or resuming debugging operations
 struct cmd_debug_stopgo_packet {
-    uint32_t stop;
+    uint32_t stop;      // Stop
 } __attribute__((packed));
-#define CMD_DEBUG_STOPGO_PACKET_SIZE 4
 
+
+// Structure for requesting thread information in debugging mode
 struct cmd_debug_thrinfo_packet {
-    uint32_t lwpid;
+    uint32_t lwpid;     // LWP ID
 } __attribute__((packed));
+
+// Response structure for thread information request
 struct cmd_debug_thrinfo_response {
-    uint32_t lwpid;
-    uint32_t priority;
-    char name[32];
-    // TODO: add more information
+    uint32_t lwpid;     // LWP ID
+    uint32_t priority;  // Priority
+    char name[32];      // Name
 } __attribute__((packed));
-#define CMD_DEBUG_THRINFO_PACKET_SIZE 4
-#define CMD_DEBUG_THRINFO_RESPONSE_SIZE 40
 
-// kern
+// Structure for reading kernel memory
 struct cmd_kern_read_packet {
-    uint64_t address;
-    uint32_t length;
+    uint64_t address;   // Address
+    uint32_t length;    // Length
 } __attribute__((packed));
-#define CMD_KERN_READ_PACKET_SIZE 12
 
+// Structure for writing to kernel memory
 struct cmd_kern_write_packet {
-    uint64_t address;
-    uint32_t length;
+    uint64_t address;   // Address
+    uint32_t length;    // Length
 } __attribute__((packed));
-#define CMD_KERN_WRITE_PACKET_SIZE 12
 
-// console
+// Structure for printing messages to console
 struct cmd_console_print_packet {
-    uint32_t length;
+    uint32_t length;    // Length
 } __attribute__((packed));
-#define CMD_CONSOLE_PRINT_PACKET_SIZE 4
 
+// Structure for sending notifications to console
 struct cmd_console_notify_packet {
-    uint32_t messageType;
-    uint32_t length;
+    uint32_t messageType;   // Message type
+    uint32_t length;        // Length
 } __attribute__((packed));
-#define CMD_CONSOLE_NOTIFY_PACKET_SIZE 8
 
+// Response structure for console information request
 struct cmd_console_info_response {
     // todo
 } __attribute__((packed));
-#define CMD_CONSOLE_INFO_RESPONSE_SIZE 8
+
 
 #define MAX_BREAKPOINTS 30
 #define MAX_WATCHPOINTS 4
-
 struct debug_breakpoint {
-    uint32_t enabled;
-    uint64_t address;
-    uint8_t original;
+    uint32_t enabled;   // Enabled
+    uint64_t address;   // Address
+    uint8_t original;   // Original
 };
 
 struct debug_watchpoint {
-    uint32_t enabled;
-    uint64_t address;
-    uint8_t breaktype;
-    uint8_t length;
+    uint32_t enabled;   // Enabled
+    uint64_t address;   // Address
+    uint8_t breaktype;  // Break type
+    uint8_t length;     // Length
 };
 
 struct debug_context {
-    int pid;
-    int dbgfd;
-    struct debug_breakpoint breakpoints[MAX_BREAKPOINTS];
+    int pid;    // Process ID
+    int dbgfd;  // Debug file descriptor
+    struct debug_breakpoint breakpoints[MAX_BREAKPOINTS]; // Breakpoints
     // XXX: use actual __dbreg64 structure please
     struct {
         uint64_t dr[16];
-    } watchdata;
+    } watchdata; // Watch data
 };
 
 struct server_client {
-    int id;
-    int fd;
-    int debugging;
-    struct sockaddr_in client;
-    struct debug_context dbgctx;
+    int id; // ID
+    int fd; // File descriptor
+    int debugging;  // Debugging
+    struct sockaddr_in client;  // Client address
+    struct debug_context dbgctx;    // Debug context
 };
 
 #endif
