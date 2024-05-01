3230983542e83a4b15958296fb0895ec18505151
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <PfizerJunkie@protonmail.com>
2024-05-01 15:02:36 +0200

    Update kern.c
    I rewrote the <data> if-statements in both kern_read_handle and kern_write_handle functions. Since pfmalloc returns NULL on error, I redid all if-statements that check the return value of pfmalloc to be if (<variable> == NULL) rather than if (!<variable>).
    I renamed the variable <rp> in kern_read_handle function to <kReadPkt>.
    I rewrote the if-statement for <kReadPkt> variable in kern_read_handle, to check if <ReadPkt> is invalid, rather than to check if it's valid and have the code inside of that if-statement.
    I renamed the variable <wp> in kern_write_handle function to <kWritePkt>.
    I rewrote the if-statement for <kWritePkt> variable in kern_write_handle, to check if <WritePkt> is invalid, rather than to check if it's valid and have the code inside of that if-statement.
    I Commented the code of both kern_write_handle and kern_read_handle functions.

--- debugger/source/kern.c
+++ debugger/source/kern.c
@@ -16,65 +16,82 @@ int kern_base_handle(int fd, struct cmd_packet *packet) {
 }
 
 int kern_read_handle(int fd, struct cmd_packet *packet) {
-    struct cmd_kern_read_packet *rp;
+    // Define variables that are going to be used
+    struct cmd_kern_read_packet *kReadPkt;
     void *data;
 
-    rp = (struct cmd_kern_read_packet *)packet->data;
-
-    if(rp) {
-        data = pfmalloc(rp->length);
-        if(!data) {
-            net_send_status(fd, CMD_DATA_NULL);
-            return 1;
-        }
-        
-        sys_kern_rw(rp->address, data, rp->length, 0);
-        
-        net_send_status(fd, CMD_SUCCESS);
-        net_send_data(fd, data, rp->length);
-
-        free(data);
-
-        return 0;
+    // TODO: Comment this part
+    kReadPkt = (struct cmd_kern_read_packet *)packet->data;
+    if (!kReadPkt) { 
+        // Send status indicating null data
+        net_send_status(fd, CMD_DATA_NULL);
+        return -1;
     }
 
-    net_send_status(fd, CMD_DATA_NULL);
+    // Try to allocate memory, that's prefaulted for the <data>
+    // variable, and if memory allocation failed handle it
+    data = pfmalloc(kReadPkt->length);
+    if (data == NULL) {
+        // Send status indicating null data
+        net_send_status(fd, CMD_DATA_NULL);
+        return -1;
+    }
 
+    // Read data from kernel memory
+    sys_kern_rw(kReadPkt->address, data, kReadPkt->length, 0);
+
+    // Send status indicating successful operation
+    net_send_status(fd, CMD_SUCCESS);
+    
+    // Send the read data
+    net_send_data(fd, data, kReadPkt->length);
+
+    // Perform cleanup, and return
+    free(data);
     return 0;
 }
 
 int kern_write_handle(int fd, struct cmd_packet *packet) {
-    struct cmd_kern_write_packet *wp;
+    // Define variables that are going to be used
+    struct cmd_kern_write_packet *kWritePkt; 
     void *data;
-
-    wp = (struct cmd_kern_write_packet *)packet->data;
-
-    if(wp) {
-        data = pfmalloc(wp->length);
-        if(!data) {
-            net_send_status(fd, CMD_DATA_NULL);
-            return 1;
-        }
-
-        net_send_status(fd, CMD_SUCCESS);
-        
-        net_recv_data(fd, data, wp->length, 1);  
-        sys_kern_rw(wp->address, data, wp->length, 1);
-
-        net_send_status(fd, CMD_SUCCESS);
-
-        free(data);
-
-        return 0;
+    
+    // TODO: Comment this part
+    kWritePkt = (struct cmd_kern_write_packet *)packet->data;
+    if (!kWritePkt) {
+        // Send status indicating null data
+        net_send_status(fd, CMD_DATA_NULL);
+        return -1;
+    }
+    
+    // Try to allocate memory, that's prefaulted for the <data>
+    // variable, and if memory allocation failed handle it
+    data = pfmalloc(kWritePkt->length);
+    if (data == NULL) {
+        // Send status indicating null data
+        net_send_status(fd, CMD_DATA_NULL);
+        return -1;
     }
 
-    net_send_status(fd, CMD_DATA_NULL);
+    // Send status indicating successful operation
+    net_send_status(fd, CMD_SUCCESS);
+    
+    // Receive write data from network
+    net_recv_data(fd, data, kWritePkt->length, 1);
 
+    // Write data to kernel memory
+    sys_kern_rw(kWritePkt->address, data, kWritePkt->length, 1); 
+
+    // Send status indicating successful operation
+    net_send_status(fd, CMD_SUCCESS);
+    
+    // Perform cleanup, and return
+    free(data);
     return 0;
 }
 
 int kern_handle(int fd, struct cmd_packet *packet) {
-    switch(packet->cmd) {
+    switch (packet->cmd) {
         case CMD_KERN_BASE:
             return kern_base_handle(fd, packet);
         case CMD_KERN_READ:
